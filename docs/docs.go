878cbf44a76a8c0bd628965a7a9eb5027f2a7f1a
Levi Gila <levi.gila@hotmail.com>
2024-08-14 15:38:11 -0300

    Atualizando o doc do Swagger

--- docs/docs.go
+++ docs/docs.go
@@ -187,9 +187,9 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "golangschoolcrud.onrender.com",
 	BasePath:         "/",
-	Schemes:          []string{},
+	Schemes:          []string{"https"},
 	Title:            "Escola CRUD API",
 	Description:      "API para gerenciar alunos em uma escola.",
 	InfoInstanceName: "swagger",
